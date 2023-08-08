87181573c485d9bfa8598de7c9aba03ff743696c
Loulian Liu <3l.liu444@gmail.com>
2023-08-08 12:17:05 -0400

    register page returns proper values

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,16 +1,59 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [registerInfo, setRegisterInfo] = useState({});
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      setRegisterInfo({
+        username,
+        password,
+      });
+      setUsername("");
+      setPassword("");
+      setConfirmPassword("");
+    } else {
+      alert(`password does not match`);
+    }
+  };
+
   return (
     <>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <label>
-          Username: <input type="text" />
+          Username:{" "}
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </label>
         <br />
         <label>
-          Password: <input type="password" />
+          Password:{" "}
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Confirm Password:{" "}
+          <input
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
         </label>
         <br />
         <button>Register</button>
