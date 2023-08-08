d92d4cca43044a279ed5ec35283c0c406c82102e
Loulian Liu <3l.liu444@gmail.com>
2023-08-08 10:39:58 -0400

    got items to show up in marketplace

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
+import Marketplace from "./components/Marketplace.jsx";
+
+const App = () => {
   return (
     <>
       <h1>Aloha World</h1>
+      <Marketplace />
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Marketplace.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
+
+const Marketplace = () => {
+  const apiUrl =
+    "https://strangers-things.herokuapp.com/api/2306-fsa-et-web-ft-sf";
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      const response = await fetch(`${apiUrl}/posts`);
+      const body = await response.json();
+
+      setProducts(body.data.posts);
+    };
+
+    fetchAllProducts();
+  }, []);
+
+  console.log(products);
+
+  return (
+    <>
+      <h1>market</h1>
+      <Grid container spacing={2}>
+        {products.map((product) => {
+          return (
+            <Grid item xs={3} key={product._id}>
+              <section className="productSection">
+                <p>
+                  <b>{product.title}</b>
+                </p>
+                <p>
+                  <b>Price</b>: {product.price}
+                </p>
+              </section>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
+
+export default Marketplace;
